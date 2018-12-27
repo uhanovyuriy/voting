fc29194a4f45128630ad78cb95429c5486cf657d
uhanovyuriy <yorikuh@gmail.com>
2018-12-27 14:58:17 +0200

    Add SecurityConfiguration and properties

---
+++ src/main/java/ru/myproject/voting/configuration/SecurityConfiguration.java
@@ -0,0 +1,48 @@
+package ru.myproject.voting.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import ru.myproject.voting.service.UserServiceImpl;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private static final String URL_REST = "/voting/rest/";
+
+    private UserServiceImpl userService;
+
+    @Autowired
+    public SecurityConfiguration(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(URL_REST + "admin/**").hasRole("ADMIN")
+                .antMatchers(URL_REST + "register/").anonymous()
+                .antMatchers(URL_REST + "voting/**").hasRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
