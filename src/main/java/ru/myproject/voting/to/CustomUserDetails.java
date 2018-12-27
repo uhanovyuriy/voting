70e1ff27a0b04d93a730e4fa67dfcb0fd0321f2d
uhanovyuriy <yorikuh@gmail.com>
2018-12-27 10:51:59 +0200

    Add CustomUserDetails

---
+++ src/main/java/ru/myproject/voting/to/CustomUserDetails.java
@@ -0,0 +1,54 @@
+package ru.myproject.voting.to;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.myproject.voting.model.User;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CustomUserDetails extends User implements UserDetails {
+
+    public CustomUserDetails(final User user) {
+        super(user);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> set = new HashSet<>();
+        super.getRoles().forEach(role -> set.add(new SimpleGrantedAuthority(role.name())));
+        return set;
+    }
+
+    @Override
+    public String getPassword() {
+        return super.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return super.isEnabled();
+    }
+}
