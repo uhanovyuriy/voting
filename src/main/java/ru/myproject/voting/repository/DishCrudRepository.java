ba0eb07eaf0dd4f3a33bcd2f042d7c80848ed99d
uhanovyuriy <yorikuh@gmail.com>
2019-01-24 14:29:09 +0200

    Fix bugs repository layer

--- src/main/java/ru/myproject/voting/repository/DishCrudRepository.java
+++ src/main/java/ru/myproject/voting/repository/DishCrudRepository.java
@@ -1,13 +1,25 @@
 package ru.myproject.voting.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import ru.myproject.voting.model.Dish;
-import ru.myproject.voting.model.Restaurant;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Transactional(readOnly = true)
 public interface DishCrudRepository extends JpaRepository<Dish, Integer> {
 
+    @Modifying
     @Transactional
-    void deleteAllByRestaurant(Restaurant restaurant);
+    @Query("DELETE FROM Dish d WHERE d.id=:id AND d.restaurant.id=:restaurantId")
+    int delete(@Param("id") int id, @Param("restaurantId") int restaurantId);
+
+    @Query("SELECT d FROM Dish d WHERE d.restaurant.id=:restaurantId AND d.created BETWEEN :startDate AND :endDate ORDER BY d.created")
+    List<Dish> getBetween(@Param("restaurantId") int restaurantId,
+                          @Param("startDate") LocalDate startDate,
+                          @Param("endDate") LocalDate endDate);
 }
--- src/main/java/ru/myproject/voting/repository/HistoryVotingCrudRepository.java
+++ src/main/java/ru/myproject/voting/repository/HistoryVotingCrudRepository.java
@@ -1,26 +1,19 @@
 package ru.myproject.voting.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import ru.myproject.voting.model.HistoryVoting;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
 @Transactional(readOnly = true)
 public interface HistoryVotingCrudRepository extends JpaRepository<HistoryVoting, Integer> {
 
-    @Query("SELECT h FROM HistoryVoting h WHERE h.user.id=:userId AND h.dateTimeVoting>:dateTime")
-    HistoryVoting getByUserIdAndDate(@Param("userId") int userId, @Param("dateTime") LocalDateTime dateTime);
+    @Query("SELECT h FROM HistoryVoting h WHERE h.user.id=:userId AND h.dateVoting=:date")
+    HistoryVoting getByUserIdAndDate(@Param("userId") int userId, @Param("date") LocalDate date);
 
-    @Query("SELECT h FROM HistoryVoting h WHERE  h.dateTimeVoting>:startDateTime AND h.dateTimeVoting<:endDateTime")
-    List<HistoryVoting> getAllToCurrentDay(@Param("startDateTime") LocalDateTime startDateTime, LocalDateTime endDateTime);
-
-    @Modifying
-    @Transactional
-    @Query("DELETE FROM HistoryVoting h WHERE h.id=:id")
-    int deleteById(@Param("id") int id);
+    List<HistoryVoting> getHistoryVotingsByDateVotingBetween(LocalDate startDate, LocalDate endDate);
 }
